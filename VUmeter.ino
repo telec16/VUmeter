23deed77c8ff251df84dc7ad8418c211e4ee20d8
telec <>
2020-05-01 23:37:41 +0200

    bargraph function

--- VUmeter.ino
+++ VUmeter.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#define SOUND A0
 
+#define LEDS 10
+const int LEDS_PINS[LEDS] = {9, 10, 11, 8, 7, 6, 5, 4, 3, 2};
+
+void setup() {
+  //analogReference(EXTERNAL);
+  
+  for(int i=0; i<LEDS; i++){
+    pinMode(LEDS_PINS[i], OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int reading = analogRead(SOUND);
+  int value = map(reading, 0, 1023, 0, 10);
+  
+  bargraph(value, 10);
 
+  delay(100);
+}
+
+void bargraph(int value, int peak){
+  --peak;
+  for(int i = 0; i < LEDS; i++){
+    digitalWrite(LEDS_PINS[i], (i < value) || (i == peak));
+  }
 }
