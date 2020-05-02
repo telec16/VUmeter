3a8f14fa24f905f85740fa5e5ea08fe7d30b4256
telec <>
2020-05-02 15:13:46 +0200

    added soft PWM

--- VUmeter.ino
+++ VUmeter.ino
@@ -1,15 +1,18 @@
 #define SOUND A0
 
+#define PWM_PERIOD 50UL //us
 #define LEDS 10
 const int LEDS_PINS[LEDS] = {9, 10, 11, 8, 7, 6, 5, 4, 3, 2};
+//0: OFF, 255: Fully ON
+const unsigned char SOFT_PWM[LEDS] = {50, 50, 50, 50, 50, 255, 255, 255, 200, 200};
 
-#define SAMPLE_PERIOD 10 //ms
-#define SAMPLES 64
+#define SAMPLE_PERIOD 10000UL //us
+#define SAMPLES 64 // => 640ms
 int samples[SAMPLES] = {0};
 int head = 0;
 int tail = 0;
-
 long sum = 0;
+
 
 void setup() {
   analogReference(EXTERNAL);
@@ -19,16 +22,32 @@ void setup() {
   }
 }
 
-void loop() {
-  int discarded = sample();
-  int current = samples[head];
-  
-  int value = map(avgSample(discarded, current), 0, 1023, 0, 10);
-  int peak = map(maxSample(), 0, 1023, 0, 10);
-  
-  bargraph(value, peak);
 
-  delay(SAMPLE_PERIOD);
+void loop() {
+  static unsigned long sampleTimer = micros();
+  static unsigned long pwmTimer = micros();
+
+  static int value = 0;
+  static int peak = 0;
+  
+  if(micros() > (sampleTimer+SAMPLE_PERIOD))
+  {
+    sampleTimer = micros();
+    
+    int discarded = sample();
+    int current = samples[head];
+    
+    value = map(current/*avgSample(discarded, current)*/, 0, 1023, 0, 10);
+    peak = map(maxSample(), 0, 1023, 0, 10);
+  }
+  
+  if(micros() > (pwmTimer+PWM_PERIOD))
+  {
+    pwmTimer = micros();
+    
+    bargraph(value, peak);
+  }
+
 }
 
 
@@ -69,8 +88,11 @@ int maxSample(){
 
 
 void bargraph(int value, int peak){
+  static unsigned char counter = 0;
+  
   --peak;
+  --counter;
   for(int i = 0; i < LEDS; i++){
-    digitalWrite(LEDS_PINS[i], (i < value) || (i == peak));
+    digitalWrite(LEDS_PINS[i], ((i < value) || (i == peak)) && (SOFT_PWM[i]>counter));
   }
 }
